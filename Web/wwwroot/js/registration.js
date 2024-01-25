ed958548db497811407078db52eac7d23df47114
Timofei <timofeibuev234@gmail.com>
2024-01-25 13:48:51 +0500

    Конец 25.01

--- Web/wwwroot/js/registration.js
+++ Web/wwwroot/js/registration.js
@@ -2,4 +2,13 @@
     let firstName = document.getElementById('firstName').value;
     alert(firstName);
 
+    const Http = new XMLHttpRequest();
+    const url = 'http://192.168.147.50\training';
+    Http.open("GET", url, false, "intern", "0000");
+    Http.send();
+
+    Http.onreadystatechange = (e) => {
+        console.log(Http.responseText);  // => получим массив данных в формате JSON
+    }
+
 }
