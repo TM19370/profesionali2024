5d688ec004bf15b014804be6fc9f035bb4a2b735
Timofei <timofeibuev234@gmail.com>
2024-01-27 13:54:44 +0500

    Конец 27.01 (почти закончил вход)

---
+++ МИС «ГКБ Большие Кабаны»/EnterWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace МИС__ГКБ_Большие_Кабаны_
+{
+    /// <summary>
+    /// Логика взаимодействия для EnterWindow.xaml
+    /// </summary>
+    public partial class EnterWindow : Window
+    {
+        public EnterWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("");
+        }
+
+        private void CloseWindowButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+    }
+}
---
+++ МИС «ГКБ Большие Кабаны»/HintClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace МИС__ГКБ_Большие_Кабаны_
+{
+    public class HintClass
+    {
+        public static String GetCaption(DependencyObject obj)
+        {
+            return (String)obj.GetValue(CaptionProperty);
+        }
+
+        public static void SetCaption(DependencyObject obj, String value)
+        {
+            obj.SetValue(CaptionProperty, value);
+        }
+
+        public static readonly DependencyProperty CaptionProperty =
+            DependencyProperty.RegisterAttached("Caption", typeof(String), typeof(EnterWindow), new UIPropertyMetadata(null));
+    }
+}
