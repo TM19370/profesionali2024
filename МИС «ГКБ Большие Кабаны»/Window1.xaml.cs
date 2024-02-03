a3abdbad38ad4bcfc1333950851ea6e9978ddb79
Timofei <timofeibuev234@gmail.com>
2024-02-03 10:21:07 +0500

    Сделал функцию «Отслеживание движения клиентов и сотрудников по помещениям больницы»

--- МИС «ГКБ Большие Кабаны»/Window1.xaml.cs
+++ МИС «ГКБ Большие Кабаны»/Window1.xaml.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics.Contracts;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +14,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace МИС__ГКБ_Большие_Кабаны_
 {
@@ -21,126 +24,86 @@ namespace МИС__ГКБ_Большие_Кабаны_
     public partial class Window1 : Window
     {
         Random random = new Random();
+        DispatcherTimer requestTimer = new DispatcherTimer();
         public Window1()
         {
             InitializeComponent();
 
-
-
+            requestTimer.Tick += request;
+            requestTimer.Interval = TimeSpan.FromSeconds(3);
+            //requestTimer.Start();                                                             нет сервера API
         }
 
-        List<Human> humansList = new List<Human>();
-        int c = 1;
-
-        private void Image_MouseDown(object sender, MouseButtonEventArgs e)
+        async void request(object sender, EventArgs e)
         {
-
-            Canvas currentCanvas = (Canvas)mg.FindName("skud16");
-            Thickness margins = currentCanvas.Margin;
-
-            if (e.ChangedButton == MouseButton.Right)//delite human
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage response = await httpClient.GetAsync("http://10.30.76.66:8082/PersonLocations");
+            string responseBody = await response.Content.ReadAsStringAsync();
+            List<PersonLocation> personsLocation = JsonConvert.DeserializeObject<List<PersonLocation>>(responseBody);
+            humansGrid.Children.Clear();
+            foreach (var personLocation in personsLocation)
             {
-                Human curPos = humansList.Where(a => a.code == Convert.ToInt32(textbox.Text)).First();
-                humansList.Remove(curPos);
-                mg.Children.Remove(curPos.ellipse);
-                return;
+                createHuman(personLocation);
             }
-            eg.Children.Clear();
-            int radius = 5;
-            int x = random.Next(5, 111);
-            int y = random.Next(5, 173);
-            while (isAnyoneAround(x, y, radius))
-            {
-                Ellipse errorEllipse = new Ellipse();
-                errorEllipse.Width = radius * 2;
-                errorEllipse.Height = radius * 2;
-                errorEllipse.VerticalAlignment = VerticalAlignment.Bottom;
-                errorEllipse.HorizontalAlignment = HorizontalAlignment.Left;
-                errorEllipse.Fill = Brushes.Red;
-                errorEllipse.Margin = new Thickness(x, 0, 0, y);
-                eg.Children.Add(errorEllipse);
-
-                x = random.Next(5, 111);
-                y = random.Next(5, 174);
-            }
-            Ellipse ellipse = new Ellipse();
-            ellipse.Width = radius * 2;
-            ellipse.Height = radius * 2;
-            ellipse.VerticalAlignment = VerticalAlignment.Bottom;
-            ellipse.HorizontalAlignment = HorizontalAlignment.Left;
-            ellipse.Fill = Brushes.Blue;
-            ellipse.Margin = new Thickness(x, 0, 0, y);
-            mg.Children.Add(ellipse);
-            humansList.Add(new Human { code = 1, ellipse = ellipse, point = new Point(x, y) });
-            c++;
         }
-
-        /*
-         x = random.Next(rooms[personCode].roomStartX, rooms[personCode].roomEndX);
-         y = random.Next(rooms[personCode].roomStartY, rooms[personCode].roomEndY);
-        */
-
-
-        List<Room> rooms = new List<Room>();
 
         void createHuman(PersonLocation personLocation)
         {
-            Canvas currentRoom = (Canvas)mg.FindName("skud" + personLocation.lastSecurityPointNumber);
+            Canvas currentRoom;
+            if (personLocation.LastSecurityPointDirection == "in")
+                currentRoom = (Canvas)mg.FindName("skud" + personLocation.lastSecurityPointNumber);
+            else if (personLocation.lastSecurityPointNumber != "0" || personLocation.lastSecurityPointNumber != "1")
+                currentRoom = skud0;
+            else
+                return;
             double width = currentRoom.Width;
             double height = currentRoom.Height;
             Thickness margins = currentRoom.Margin;
-            int personCode = personLocation.personCode;
 
             int radius = 5;
-            int x = random.Next((int)margins.Left + 5,(int)margins.Left - 5 + (int)width);
-            int y = random.Next((int)margins.Bottom + 5, (int)margins.Bottom - 5 + (int)height);
-            while (isAnyoneAround(x, y, radius))
+            int yMin = (int)margins.Bottom + radius + 5;
+            int yMax = (int)margins.Bottom - radius - 5 + (int)height;
+            int xMin = (int)margins.Left + radius + 5;
+            int xMax = (int)margins.Left - radius - 5 + (int)width;
+            int x;
+            int y;
+            do
             {
-                x = random.Next(rooms[personCode].roomStartX, rooms[personCode].roomEndX);
-                y = random.Next(rooms[personCode].roomStartY, rooms[personCode].roomEndY);
+                x = random.Next(xMin, xMax);
+                y = random.Next(yMin, yMax);
             }
+            while (isAnyoneAround(x, y, radius));
             Ellipse ellipse = new Ellipse();
             ellipse.Width = radius * 2;
             ellipse.Height = radius * 2;
             ellipse.VerticalAlignment = VerticalAlignment.Bottom;
             ellipse.HorizontalAlignment = HorizontalAlignment.Left;
-            ellipse.Fill = Brushes.Blue;
+            if (personLocation.personRole == "Сотрудник")
+                ellipse.Fill = Brushes.Blue;
+            else
+                ellipse.Fill = Brushes.Green;
             ellipse.Margin = new Thickness(x, 0, 0, y);
-            mg.Children.Add(ellipse);
-            humansList.Add(new Human { code = personLocation.personCode, ellipse = ellipse, point = new Point(x, y) });
-        }
-
-        void deliteHuman(PersonLocation personLocation)
-        {
-            Human curPos = humansList.Where(x => x.code == personLocation.personCode).First();
-            humansList.Remove(curPos);
-            mg.Children.Remove(curPos.ellipse);
+            humansGrid.Children.Add(ellipse);
         }
 
         bool isAnyoneAround(int x,int y, int radius)
         {
-            var aroundList = new List<Human>();
-            foreach (var human in humansList)
+            var humans = humansGrid.Children;
+            foreach (Ellipse human in humans)
             {
-                double gip = Math.Sqrt(Math.Pow(Math.Abs(x - human.point.X), 2) + Math.Pow(Math.Abs(y - human.point.Y), 2));
-                if(gip <= radius * 2)
+                double gip = Math.Sqrt(Math.Pow(Math.Abs(x - human.Margin.Left), 2) + Math.Pow(Math.Abs(y - human.Margin.Bottom), 2));
+                if (gip <= radius * 2)
                 {
-                    aroundList.Add(human);
+                    return true;
                 }
             }
-            if (aroundList.Count > 0)
-                return true;
-            else
-                return false;
+            return false;
         }
-    }
 
-    class Room
-    {
-        public int roomStartX {  get; set; }
-        public int roomEndX { get; set; }
-        public int roomStartY { get; set; }
-        public int roomEndY { get; set; }
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            requestTimer.Stop();
+        }
     }
 
     class PersonLocation
@@ -148,13 +111,7 @@ namespace МИС__ГКБ_Большие_Кабаны_
         public int personCode { get; set; }
         public string personRole { get; set; }
         public string lastSecurityPointNumber { get; set; }
+        public string LastSecurityPointDirection { get; set; }
         public string lastSecurityPointTime { get; set; }
-    }
-
-    class Human
-    {
-        public int code { get; set; }
-        public Ellipse ellipse { get; set; }
-        public Point point { get; set; }
     }
 }
